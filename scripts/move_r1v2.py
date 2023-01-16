3cfa6c0ff7c8ea185aaf2b6f64ab9b3ad8f905f3
James McGann <jamesm@ATX-MBP-RQ05P.lan>
2023-01-16 15:43:44 -0500

    Update test scripts.

--- scripts/move_r1v2.py
+++ scripts/move_r1v2.py
@@ -1,27 +1,17 @@
 '''Testing move of r2 from original location to cell-ranger_input'''
 
 import os
-from pathlib import Path
 
 from latch import small_task
-from latch.types import LatchDir, LatchFile
-
-# @small_task
-# def copy_r1(r1: LatchFile) -> (LatchFile):
-
-#         return LatchFile(r1.local_path, "latch:///cell-ranger_inputs/S3_L001_R1_001.fastq.gz")
-
-# if __name__ == '__main__':
-#         copy_r1(
-#         r1=LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/D01033_NG01681_S3_L001_R1_001.fastq.gz")
-#         )
+from latch.types import LatchFile
 
 @small_task
 def copy_r1(r1: LatchFile) -> (LatchFile):
 
-        return LatchFile(r1.local_path, "latch:///cell-ranger_inputs/foo.txt")
+        return LatchFile(r1.local_path, "latch:///cell-ranger_inputs/S3_L001_R1_001.fastq.gz")
 
 if __name__ == '__main__':
         copy_r1(
-        r1=LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/foo.txt")
+        r1=LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/D01033_NG01681_S3_L001_R1_001.fastq.gz")
         )
+
