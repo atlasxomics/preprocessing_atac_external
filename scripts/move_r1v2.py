5d40c587f3888219c1ef9ff40ccee290ae495f7d
James McGann <jamesm@ATX-MBP-RQ05P.lan>
2023-01-16 14:23:51 -0500

    Add scripts for testing copying r1.

---
+++ scripts/move_r1v2.py
@@ -0,0 +1,27 @@
+'''Testing move of r2 from original location to cell-ranger_input'''
+
+import os
+from pathlib import Path
+
+from latch import small_task
+from latch.types import LatchDir, LatchFile
+
+# @small_task
+# def copy_r1(r1: LatchFile) -> (LatchFile):
+
+#         return LatchFile(r1.local_path, "latch:///cell-ranger_inputs/S3_L001_R1_001.fastq.gz")
+
+# if __name__ == '__main__':
+#         copy_r1(
+#         r1=LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/D01033_NG01681_S3_L001_R1_001.fastq.gz")
+#         )
+
+@small_task
+def copy_r1(r1: LatchFile) -> (LatchFile):
+
+        return LatchFile(r1.local_path, "latch:///cell-ranger_inputs/foo.txt")
+
+if __name__ == '__main__':
+        copy_r1(
+        r1=LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/foo.txt")
+        )
