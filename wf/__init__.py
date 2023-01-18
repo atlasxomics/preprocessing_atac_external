ee48322470f8949f3bbf2ba3347cd16ed81eeb54
James McGann <jamesm@ATX-MBP-RQ05P.local>
2023-01-17 19:06:44 -0500

    Add 'run' parameter and file structure to wf.

--- wf/__init__.py
+++ wf/__init__.py
@@ -8,96 +8,121 @@ from latch.types import LatchAuthor, LatchDir, LatchFile, LatchMetadata, LatchPa
 
 
 @large_task(cache=True)
-def filter_r2_task(r1: LatchFile, r2: LatchFile) -> (LatchFile, LatchFile):
+def filter_task(
+    r1: LatchFile,
+    r2: LatchFile,
+    run: str
+) -> (LatchFile, LatchFile):
 
-    bbduk1_cmd = [
+    filtered_r1_l1 = Path(f"{run}_linker1_R1.fastq.gz").resolve()
+    filtered_r2_l1 = Path(f"{run}_linker1_R2.fastq.gz").resolve()
+
+    _bbduk1_cmd = [
         "bbmap/bbduk.sh",
         f"in1={r1.local_path}",
         f"in2={r2.local_path}",
-        "outm1=/root/D01033_NG01681_linker1_R1.fastq.gz",
-        "outm2=/root/D01033_NG01681_linker1_R2.fastq.gz",
+        f"outm1={str(filtered_r1_l1)}",
+        f"outm2={str(filtered_r2_l1)}",
         "skipr1=t",
         "k=30",
         "mm=f",
         "rcomp=f",
         "restrictleft=103",
         "hdist=3",
-        "stats=/root/D01033_NG01681_linker1_stats.txt",
+        f"stats=/root/{run}_linker1_stats.txt",
         "threads=46",
         "literal=GTGGCCGATGTTTCGCATCGGCGTACGACT"
         ]
 
-    subprocess.run(bbduk1_cmd)
+    subprocess.run(_bbduk1_cmd)
 
-    bbduk2_cmd = [
+    filtered_r1_l2 = Path(f"{run}_linker2_R1.fastq.gz").resolve()
+    filtered_r2_l2 = Path(f"{run}_linker2_R2.fastq.gz").resolve()
+
+    _bbduk2_cmd = [
         "bbmap/bbduk.sh",
-        "in1=/root/D01033_NG01681_linker1_R1.fastq.gz",
-        "in2=/root/D01033_NG01681_linker1_R2.fastq.gz",
-        "outm1=/root/D01033_NG01681_linker2_R1.fastq.gz",
-        "outm2=/root/D01033_NG01681_linker2_R2.fastq.gz",
+        f"in1={str(filtered_r1_l1)}",
+        f"in2={str(filtered_r2_l1)}",
+        f"outm1={str(filtered_r1_l2)}",
+        f"outm2={str(filtered_r2_l2)}",
         "skipr1=t",
         "k=30",
         "mm=f",
         "rcomp=f",
         "restrictleft=65",
         "hdist=3",
-        "stats=/root/D01033_NG01681_linker2_stats.txt",
+        f"stats=/root/{run}_linker2_stats.txt",
         "threads=46",
         "literal=ATCCACGTGCTTGAGAGGCCAGAGCATTCG"
         ]
 
-    subprocess.run(bbduk2_cmd)
-
-    filtered_r1 = Path("D01033_NG01681_linker2_R1.fastq.gz").resolve()
-    filtered_r2 = Path("D01033_NG01681_linker2_R2.fastq.gz").resolve()
+    subprocess.run(_bbduk2_cmd)
 
     return (
-            LatchFile(str(filtered_r1), f"latch:///preprocessing/D01033_NG01681_linker2_R1.fastq.gz"),
-            LatchFile(str(filtered_r2), f"latch:///preprocessing/D01033_NG01681_linker2_R2.fastq.gz")
+            LatchFile(
+                str(filtered_r1_l2),
+                f"latch:///runs/{run}/preprocessing/{run}_linker2_R1.fastq.gz"
+            ),
+            LatchFile(
+                str(filtered_r2_l2),
+                f"latch:///runs/{run}/preprocessing/{run}_linker2_R2.fastq.gz"
+            )
         )
 
 @large_task(cache=True)
-def process_bc_task(r2: LatchFile) -> (LatchFile, LatchFile):
+def process_bc_task(r2: LatchFile, run: str) -> (LatchFile, LatchFile):
     """ Process read2: save genomic portion as read3, extract
     barcode seqs and save as read3
     """
 
-    bc_cmd = [
+    new_r2 = Path(f"{run}_S1_L001_R2_001.fastq").resolve()
+    r3 = Path(f"{run}_S1_L001_R3_001.fastq").resolve()
+
+    _bc_cmd = [
         "python",
         "bc_process.py",
         "--input",
         r2.local_path,
         "--output_R2",
-        "/root/D01033_NG01681_S1_L001_R2_001.fastq",
+        f"{str(new_r2)}",
         "--output_R3",
-        "/root/D01033_NG01681_S1_L001_R3_001.fastq"
+        f"{str(r3)}"
         ]
 
-    subprocess.run(bc_cmd)
-
-    file_r2 = Path("D01033_NG01681_S1_L001_R2_001.fastq").resolve()
-    file_r3 = Path("D01033_NG01681_S1_L001_R3_001.fastq").resolve()
+    subprocess.run(_bc_cmd)
 
     return ( 
-        LatchFile(str(file_r2), f"latch:///cell-ranger_inputs/D01033_NG01681_S1_L001_R2_001.fastq"),
-        LatchFile(str(file_r3), f"latch:///cell-ranger_inputs/D01033_NG01681_S1_L001_R3_001.fastq")
+        LatchFile(
+            str(new_r2),
+            f"latch:///runs/{run}/cellranger_inputs/{run}_S1_L001_R2_001.fastq"
+        ),
+        LatchFile(
+            str(r3),
+            f"latch:///runs/{run}/cellranger_inputs/{run}_S1_L001_R3_001.fastq"
+        )
     )
 
 @large_task(cache=True)
-def copy_r1_task(r1: LatchFile) -> (LatchFile):
+def copy_r1_task(r1: LatchFile, run: str) -> (LatchFile):
 
-        return LatchFile(r1.local_path, "latch:///cell-ranger_inputs/D01033_NG01681_S1_L001_R1_001.fastq.gz")
+    return LatchFile(
+        r1.local_path,
+        f"latch:///runs/{run}/cellranger_inputs/{run}_S1_L001_R1_001.fastq.gz"
+        )
 
 @large_task
 def cellranger_task(
-    input_dir: LatchDir = "latch:///cell-ranger_inputs",
-    output_dir: LatchDir = "latch:///outs"
+    input_dir: LatchDir,
+    output_dir: LatchDir,
+    run: str
 ) -> (LatchDir):
+
+    local_out = Path(f'{run}/outs/').resolve()
 
     cr_command = [
     "cellranger-atac-2.1.0/cellranger-atac",
     "count",
-    "--id=D01033_NG01681", # this will need to be dynamic
+    f"--id={run}", 
     "--reference=refdata-cellranger-arc-GRCh38-2020-A-2.0.0",
     f"--fastqs={input_dir.local_path}",
     "--localcores=96",
@@ -107,30 +132,34 @@ def cellranger_task(
 
     subprocess.run(cr_command)
 
-    cr_outputdir = Path('/root/D01033_NG01681/outs').resolve()
-
-    return LatchDir(cr_outputdir, output_dir.remote_path)
+    return LatchDir(local_out, output_dir.remote_path)
 
 """The metadata included here will be injected into your interface."""
 metadata = LatchMetadata(
-    display_name="ATX-test-2",
+    display_name="Spatial ATAC-seq",
     documentation="your-docs.dev",
     author=LatchAuthor(
         name="James McGann",
         email="jamesm@atlasxomics.com",
         github="github.com/jpmcga",
     ),
-    repository="https://github.com/jpmcga/Spatial_ATAC-seq/",
+    repository="https://github.com/jpmcga/spatial-atacseq_latch/",
     license="MIT",
     parameters={
         "r1": LatchParameter(
-            display_name="Read 1",
-            description="Paired-end read 1 file to be processed.",
+            display_name="read 1",
+            description="Read 1 must contain genomic sequence.",
             batch_table_column=True,  # Show this parameter in batched mode.
         ),
         "r2": LatchParameter(
-            display_name="Read 2",
-            description="Paired-end read 2 file to be processed.",
+            display_name="read 2",
+            description="Read 2 must contain barcode sequences \
+                        and end with >35bp of genomic sequence.",
+            batch_table_column=True,  # Show this parameter in batched mode.
+        ),
+        "run": LatchParameter(
+            display_name="run",
+            description="Name of run, defailt to Dxxxxx_NGxxxxx format.",
             batch_table_column=True,  # Show this parameter in batched mode.
         ),
     },
@@ -138,7 +167,11 @@ metadata = LatchMetadata(
 )
 
 @workflow(metadata)
-def spatial_atac(r1: LatchFile, r2: LatchFile) -> (LatchFile, LatchFile, LatchFile):
+def spatial_atac(
+    r1: LatchFile,
+    r2: LatchFile,
+    run: str
+) -> (LatchDir):
     """Description...
 
     Spatial ATAC-seq
@@ -156,11 +189,15 @@ def spatial_atac(r1: LatchFile, r2: LatchFile) -> (LatchFile, LatchFile, LatchFi
     * run Cell Ranger ATAC
     """
 
-    filtered_r1, filtered_r2 = filter_r2_task(r1=r1, r2=r2)
-    new_r2, r3 = process_bc_task(r2=filtered_r2)
-    new_r1 = copy_r1_task(r1=filtered_r1)
-    # cellranger_task(input_dir="latch:///cell-ranger_inputs", output_dir="latch:///outs")
-    return new_r1, new_r2, r3
+    filtered_r1, filtered_r2 = filter_task(r1=r1, r2=r2, run=run)
+    new_r2, r3 = process_bc_task(r2=filtered_r2, run=run)
+    new_r1 = copy_r1_task(r1=filtered_r1, run=run)
+    
+    return cellranger_task(
+        input_dir=f"latch:///runs/{run}/cellranger_inputs/",
+        output_dir=f"latch:///runs/{run}/outs/"
+        )
+     
 
 """
 Add test data with a LaunchPlan. Provide default values in a dictionary with
@@ -171,7 +208,8 @@ LaunchPlan(
     spatial_atac,
     "Test Data",
     {
-        "r1": LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/D01033_NG01681_S3_L001_R1_001.fastq.gz"),
-        "r2": LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/D01033_NG01681_S3_L001_R2_001.fastq.gz"),
+        "r1" : LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/D01033_NG01681_S3_L001_R1_001.fastq.gz"),
+        "r2" : LatchFile("latch:///BASESPACE_IMPORTS/projects/PL000121/D01033_NG01681_L1/D01033_NG01681_S3_L001_R2_001.fastq.gz"),
+        "run" : "D01033_NG01681"
     },
 )
